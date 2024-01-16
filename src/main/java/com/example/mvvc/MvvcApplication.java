63c913f1bec1dab626afd67aecdaea9f37321829
hyungeunShin <123938245+hyungeunShin@users.noreply.github.com>
2024-01-16 23:50:36 +0900

    2024-01-16

---
+++ src/main/java/com/example/mvvc/MvvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.mvvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvvcApplication.class, args);
+	}
+
+}
